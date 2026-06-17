c1a7484eeaade0bbacde60f8548c81d0978cb072
dcgercossimo <dcgercossimo@gmail.com>
2026-06-16 22:59:00 -0300

    init: initial commit

---
+++ src/test/java/br/com/aurealab/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aurealab.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
